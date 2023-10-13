7c2cce9dc838302a147ff3522ebf66b9921359fb
nhs0912 <nhs0912@nate.com>
2023-10-13 16:56:52 +0900

    seconds commit

---
+++ src/main/java/com/justone/fileencoding/ChangeEncodingApplication.java
@@ -0,0 +1,13 @@
+package com.justone.fileencoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeEncodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeEncodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justone/fileencoding/ChangeEncodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justone.fileencoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeEncodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
